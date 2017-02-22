56fe3208f5b9638551119f3a6c4c6d9f7aa23cf4
林金义 <linjinyi@apexsoft.com.cn>
2017-02-22 10:21:48 +0800

    Dialog新增props: scrollToKeyboardOnFocus, 在输入框在获取焦点时，会自动滚动Dialog到键盘之上

--- src/Dialog/DialogAnimation.js
+++ src/Dialog/DialogAnimation.js
@@ -2,10 +2,19 @@
 'use strict';
 
 import React, {Component, PropTypes} from 'react';
-import ReactNative, {View, Text} from 'react-native';
+import ReactNative, {View, ScrollView, UIManager, findNodeHandle} from 'react-native';
 var Animation = require('../Popover/Animation');
 
 class DialogAnimation extends Animation {
+	static propTypes = {
+		scrollToKeyboardOnFocus: PropTypes.bool,
+	};
+
+	static defaultProps = {
+		...Animation.defaultProps,
+		scrollToKeyboardOnFocus: true,
+	};
+
 	prepareStyle() {
 		let {layout, anim} = this.state;
 		return {
@@ -15,6 +24,51 @@ class DialogAnimation extends Animation {
 			],
 		};
 	}
+
+	onKeyboardWillShow = () => {
+		const {scrollView, anchor} = this.refs;
+
+		scrollView.additionalScrollOffset = 10;
+		scrollView.preventNegativeScrollOffset = true;
+		UIManager.measureInWindow(
+			findNodeHandle(anchor), 
+			scrollView.scrollResponderInputMeasureAndScrollToKeyboard
+		);
+	}
+
+	onKeyboardWillHide = () => {
+		this.refs.scrollView.scrollTo({y: 0});
+	}
+
+
+	render() {
+		if(!this.props.scrollToKeyboardOnFocus) {
+			return super.render();
+		}
+
+		return (
+			<ScrollView
+				ref='scrollView'
+				style={styles.scrollview}
+				onKeyboardWillShow={this.onKeyboardWillShow}
+				onKeyboardWillHide={this.onKeyboardWillHide}
+				keyboardShouldPersistTaps='always'
+				automaticallyAdjustContentInsets={false}
+				scrollEnabled={false}
+			>
+				{super.render()}
+				<View ref='anchor' />
+			</ScrollView>
+		);
+	}
 }
+
+const styles = {
+	scrollview: {
+		flex: 0,
+		flexGrow: 0,
+    	overflow: 'visible',
+	},
+};
 
 module.exports = DialogAnimation;
